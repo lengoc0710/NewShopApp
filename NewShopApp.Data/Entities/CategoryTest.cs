df695a17a0b1ea475fa8243e1616678dc07e510d
NGOC LE BAO <quanngoc2026@gmail.com>
2022-04-22 16:57:00 +0700

    Add Entity

---
+++ NewShopApp.Data/Entities/CategoryTest.cs
@@ -0,0 +1,16 @@
+﻿using NewShopApp.Data.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewShopApp.Data.Entities
+{
+    public class CategoryTest
+    {
+        public int ID { set; get; }
+        public int SortOrder { set; get; }
+        public int OnHome { set; get; }
+        public int? ParentID { set; get; }
+        public Status Status { set; get; }
+    }
+}
